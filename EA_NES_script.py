b9a3f8f9f42521d4e3cd60e97cef627e64bca2af
jdm <jasperdemeijer@gmail.com>
2021-05-24 01:37:14 +0200

    added result logger and structure to solve NAN

--- EA_NES_script.py
+++ EA_NES_script.py
@@ -9,9 +9,12 @@
 
 
 import numpy as np
+np.seterr(all='raise') #raise errors instead of printing a warning in order to catch them as an exception.
 import argparse
+import warnings
 from game import Game
 from players import AI
+
 
 
 import concurrent.futures
@@ -22,13 +25,15 @@ import concurrent.futures
 
 class NES:
 
-  def __init__(self, weights, steps, sigma, learningrate, population, piecelimit):
+  def __init__(self, weights, steps, sigma, learningrate, population, piecelimit, run, log):
     self.weights = weights
     self.steps = steps
     self.sigma = sigma
     self.learningrate = learningrate
     self.population = population
     self.piecelimit = piecelimit
+    self.run = run
+    self.log = log
 
   def runTetris(self, weights = None):
     player = AI(weights)
@@ -37,8 +42,17 @@ class NES:
     reward = game.run_game()
     return reward
 
+  def log_results(self, reward, iteration):
+
+  	with open('NES_results/'+ str(self.run), 'a') as file:
+  		file.write(str(iteration) + ',' + str(self.weights) + ',' + str(reward) + '\n')
+
+
+
+
   def optimize(self):
     for i in range(self.steps):
+
       
     
       N = np.random.randn(self.population, len(self.weights))
@@ -62,39 +76,56 @@ class NES:
         for t in processList:
          
           X.append(t.result())
-          
-       
-      standardized_rewards = (X - np.mean(X)) / np.std(X)
+      
+      #Try and catch for calculating the gradient in case of rewards being 0.
+      try:
+      	standardized_rewards = (X - np.mean(X)) / np.std(X)
+      	grad = np.dot(N.T, standardized_rewards)/(population * sigma)
+
+      except FloatingPointError: #Flawed solution to the problem, need to think about it
+      	X = [x + 0.0001 for x in X]
+      	standardized_rewards = (X - np.mean(X)) / np.std(X)
+      	grad = np.dot(N.T, standardized_rewards)/(population * sigma)
+      	print('Flawed solution', grad)
       
       #standardized_rewards = (R - np.mean(R)) / np.std(R)
-      grad = np.dot(N.T, standardized_rewards)/(population * sigma)
+      
       self.weights += self.learningrate * grad
 
-
+      reward = self.runTetris(tuple(self.weights))
       if i % 1 == 0:
         print('iter %d. w: %s, reward: %d' % 
-          (i, str(self.weights), self.runTetris(tuple(self.weights))))
+          (i, str(self.weights), reward))
+      if self.log == True:
+      	self.log_results(reward, i)
+
 
 
 
 # In[12]:
 
 
-weights = [0.3, -0.4, -0.5, -0.3, -0.4, -0.5, -0.1, 0.4] #[-1, -1, -1, 1]
-steps = 300
+weights = [0.3, -0.4, -0.5, -0.3, -0.4, -0.5, -0.1, 0.4] #working weights
+steps = 5
 sigma = 0.1
 learningrate = 0.01
 population = 50
 piecelimit = -1
+runs = 5
+log = True
+experiment_name = 'test'
 
 
 
-samplerun = NES(weights, steps, sigma, learningrate, population, piecelimit)
-samplerun.optimize()
+
+for run in range(runs):
+	experiment = str(run) + '_' + experiment_name
+	weights = np.random.uniform(low = -1.5, high = 1.5, size= (8,))
+	samplerun = NES(weights, steps, sigma, learningrate, population, piecelimit, experiment, log)
+	samplerun.optimize()
 
 
 # In[ ]:
-
 
 
 
