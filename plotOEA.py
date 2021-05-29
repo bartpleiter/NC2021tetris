4de71b5b881d6a9b00468bb36713a81702f72d37
Bart <bart@b4rt.nl>
2021-05-29 13:28:17 +0200

    Improved OEA plot script. Added results and plots

--- plotOEA.py
+++ plotOEA.py
@@ -7,11 +7,23 @@ import matplotlib.pyplot as plt
 import sys
 from os import listdir
 from os.path import isfile, join
+import scipy.stats as st
 
+if len(sys.argv) < 3:
+    print("Please give a log filter argument (specifying which logs to read), and an EXP number for in the plot titles")
+    exit()
+
+EXP_NR = sys.argv[2]
 
 # get all logs from folder
-allLogFiles = [f for f in listdir("OEA_results") if isfile(join("OEA_results", f))]
+allLogFiles = [f for f in listdir("OEA_results") if isfile(join("OEA_results", f)) and sys.argv[1] in f and "_times" not in f]
 allLogFiles.sort()
+
+print("Found", len(allLogFiles), "logs")
+
+if len(allLogFiles) == 0:
+    print("Found no matching logs")
+    exit()
 
 
 # parse log files into lists
@@ -46,6 +58,7 @@ parsedLogs = [parseLog(f) for f in allLogFiles]
 
 
 
+CONFIDENCE_INTERVAL = 0.95
 GENERATIONS = list(range(len(parsedLogs[0])))
 RUNS = len(parsedLogs)
 IDX_GEN = 0
@@ -54,13 +67,14 @@ IDX_MAXSCORE = 2
 IDX_AVGWEIGHT = 3
 IDX_AVGSCORE = 4
 WEIGHTNAMES = ["FullRows", "Holes", "HoleDepth", "Bumpiness", "DeepWells", "DeltaHeight", "ShallowWells", "PatternDiversity"]
-
+COLORS = ["blue", "orange", "green", "red", "purple", "brown", "violet", "grey"]
 
 
 # plot weights
 
 meanWeightList = []
 stdWeightList = []
+plt.figure()
 
 for gen in GENERATIONS:
     weights = [x[IDX_AVGWEIGHT] for x in [run[gen] for run in parsedLogs]]
@@ -74,14 +88,19 @@ for gen in GENERATIONS:
     meanWeightList.append(meanWeights)
     stdWeightList.append(stdWeights)
 
-for w in range(len(meanWeightList[0])):
-    plt.errorbar(GENERATIONS, [x[w] for x in meanWeightList], yerr=[x[w] for x in stdWeightList], label=WEIGHTNAMES[w]) 
+for i, w in enumerate(range(len(meanWeightList[0]))):
+    plt.plot(GENERATIONS, [x[w] for x in meanWeightList], label=WEIGHTNAMES[w], color=COLORS[i]) 
+    mean = np.array([x[w] for x in meanWeightList])
+    std = np.array([x[w] for x in stdWeightList])
+    plt.fill_between(GENERATIONS, mean-std, mean+std, color=COLORS[i], alpha=.05)
     plt.xlabel("Generation")
     plt.ylabel("Weight value")
-    plt.title("Avg weights for each generation, with std error over " + str(RUNS) + " runs")
+    plt.title("EXP "+EXP_NR+"\nWeights of average individual, averaged over " + str(RUNS) + " runs, with STD error")
 
 plt.legend()
-plt.show()
+plt.savefig("Plots/Optimized_EXP "+EXP_NR+ "_"+ sys.argv[1] +"_avgWeights.png")
+#plt.show()
+plt.figure()
 
 
 # plot weights
@@ -101,69 +120,73 @@ for gen in GENERATIONS:
     meanWeightList.append(meanWeights)
     stdWeightList.append(stdWeights)
 
-for w in range(len(meanWeightList[0])):
-    plt.errorbar(GENERATIONS, [x[w] for x in meanWeightList], yerr=[x[w] for x in stdWeightList], label=WEIGHTNAMES[w]) 
+for i, w in enumerate(range(len(meanWeightList[0]))):
+    plt.plot(GENERATIONS, [x[w] for x in meanWeightList], label=WEIGHTNAMES[w], color=COLORS[i]) 
+    mean = np.array([x[w] for x in meanWeightList])
+    std = np.array([x[w] for x in stdWeightList])
+    plt.fill_between(GENERATIONS, mean-std, mean+std, color=COLORS[i], alpha=.05)
     plt.xlabel("Generation")
     plt.ylabel("Weight value")
-    plt.title("Max weights for each generation, with std error over " + str(RUNS) + " runs")
+    plt.title("EXP "+EXP_NR+"\nWeights of best individual, averaged over " + str(RUNS) + " runs, with STD error")
 
 plt.legend()
-plt.show()
-
+plt.savefig("Plots/Optimized_EXP "+EXP_NR+ "_"+ sys.argv[1] +"_bestWeights.png")
+#plt.show()
+plt.figure()
 
 # plot scores
 meanScores = []
 stdScores = []
+rangeScores = []
 
 for gen in GENERATIONS:
     scores = [x[IDX_MAXSCORE] for x in [run[gen] for run in parsedLogs]]
     meanScores.append(np.mean(scores))
     stdScores.append(np.std(scores))
+    rangeScores.append((np.min(scores), np.max(scores)))
+    
 
-plt.errorbar(GENERATIONS, meanScores, yerr=stdScores) 
+lowestScores = [c[0] for c in rangeScores]
+highestScores = [c[1] for c in rangeScores]
+
+stdScores = np.array(stdScores)
+meanScores = np.array(meanScores)
+
+plt.plot(GENERATIONS, meanScores) 
+plt.fill_between(GENERATIONS, meanScores, meanScores + stdScores, color='r', alpha=.1) 
+plt.fill_between(GENERATIONS, lowestScores, meanScores, color='b', alpha=.1) 
 plt.xlabel("Generation")
-plt.ylabel("Weight")
-plt.title("Max score for each generation, with std error over " + str(RUNS) + " runs")
-plt.show()
+plt.ylabel("Score")
+plt.title("EXP "+EXP_NR+": Max score for each generation averaged over " + str(RUNS) + " runs, \nthe upper STD in red, and the lower bound in blue")
+plt.savefig("Plots/Optimized_EXP "+EXP_NR+ "_"+ sys.argv[1] +"_maxScore.png")
+#plt.show()
+plt.figure()
 
 
 
-
-
-# plot avg scores
+# plot scores
 meanScores = []
 stdScores = []
+rangeScores = []
 
 for gen in GENERATIONS:
     scores = [x[IDX_AVGSCORE] for x in [run[gen] for run in parsedLogs]]
     meanScores.append(np.mean(scores))
     stdScores.append(np.std(scores))
+    rangeScores.append((np.min(scores), np.max(scores)))
+    
 
-plt.errorbar(GENERATIONS, meanScores, yerr=stdScores) 
+lowestScores = [c[0] for c in rangeScores]
+highestScores = [c[1] for c in rangeScores]
+
+stdScores = np.array(stdScores)
+meanScores = np.array(meanScores)
+
+plt.plot(GENERATIONS, meanScores) 
+plt.fill_between(GENERATIONS, meanScores, meanScores + stdScores, color='r', alpha=.1) 
+plt.fill_between(GENERATIONS, lowestScores, meanScores, color='b', alpha=.1) 
 plt.xlabel("Generation")
-plt.ylabel("Weight")
-plt.title("Average score for each generation, with std error over " + str(RUNS) + " runs")
-plt.show()
-
-
-
-
-
-
-
-"""
-# plot weights
-weightNames = ["FullRows", "Holes", "HoleDepth", "Bumpiness", "DeepWells", "DeltaHeight", "ShallowWells", "PatternDiversity"]
-for i in range(len(weightNames)):
-
-    for idx, log in enumerate(parsedLogs):
-        gen = [l[0] for l in log]
-        weight = [l[1][i] for l in log]
-        plt.plot(gen, weight, label = "run " + str(idx + 1))
-
-plt.xlabel("Generation")
-plt.ylabel("Weight")
-plt.title(weightNames[i] + " weight per generation for each run")
-plt.legend()
-plt.show()
-"""
+plt.ylabel("Score")
+plt.title("EXP "+EXP_NR+": Mean score for each generation averaged over " + str(RUNS) + " runs, \nthe upper STD in red, and the lower bound in blue")
+plt.savefig("Plots/Optimized_EXP "+EXP_NR+ "_"+ sys.argv[1] +"_meanScore.png")
+#plt.show()
